264faa04195ea6885fca34c99535b1aa1bea24b9
Khadim Hussain <khadimkalhoro1@gmail.com>
2024-05-26 12:18:39 +0500

    get all videos api done

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,17 +9,62 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+
+  const queryObject = {};
+  if (query) {
+    queryObject.title = { $regex: query, $options: "i" };
+  }
+  if (userId) {
+    queryObject.owner = userId;
+  }
+  const sort = {};
+  if (sortBy) {
+    sort[sortBy] = sortType || "asc";
+  }
+  const skip = (page - 1) * limit;
+
+  const videos = await Video.find(queryObject)
+    .sort(sort)
+    .skip(skip)
+    .limit(limit);
+  const count = await Video.countDocuments(queryObject);
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      videos,
+      pagination: { page, limit, totalPages: Math.ceil(count / limit) },
+    })
+  );
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
+
   if (!title || !description) {
     throw new ApiError(400, "Title and Description must be provided");
   }
 
-  const videoFileLocalPath = req.files?.videoFile[0]?.path;
-  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+  // const videoFileLocalPath = req.files?.videoFile[0].path || null;
+  let videoFileLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.videoFile) &&
+    req.files.videoFile.length > 0
+  ) {
+    videoFileLocalPath = req.files.videoFile[0].path;
+  }
+  // const thumbnailLocalPath = req.files?.thumbnail[0].path || null;
+  let thumbnailLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.thumbnail) &&
+    req.files.thumbnail.length > 0
+  ) {
+    thumbnailLocalPath = req.files.thumbnail[0].path;
+  }
+  console.log("publish video controller");
+
   if (!videoFileLocalPath || !thumbnailLocalPath) {
     throw new ApiError(400, "Video and thumbnail files must be provided");
   }
